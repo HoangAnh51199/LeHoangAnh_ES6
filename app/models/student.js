20f7acb0ba9385f15f8bf1c5392dcc1a45866302
HoangAnh <hoanganh51199@gmail.com>
2023-08-09 21:55:34 +0700

    lan 2

--- app/models/student.js
+++ app/models/student.js
@@ -1,17 +1,17 @@
 import { Person } from "./person.js";
 
 export class Student extends Person {
-    constructor(id, name, address, email, type, math, physicial, chemistry,DTB) {
+    constructor(id, name, address, email, type, math, physicial, chemistry) {
         super(id, name, address, email, type);
 
         this.math = math;
         this.physicial = physicial;
         this.chemistry = chemistry;
-        this.DTB=DTB;
+       
     }
 
     tinhDTB() {
-        this.DTB = this.math + this.physicial + this.chemistry;
+        return (this.math + this.physicial + this.chemistry) / 3; // %3 lấy phần dư 
     }
 
 }
